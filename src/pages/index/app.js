4f2cbef6b092d5affcce7515711ed4ff38e28ac1
BiletskiyAndery <pro100bejika@gmail.com>
2018-12-15 00:22:58 +0200

    add second sass

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -1,9 +1,18 @@
 import '../../assets/css/style.sass'
 import 'bootstrap'
 
-var width, height, mainContent;
-//получаем данние о экране
-width = screen.width;
-height = screen.height;
-mainContent = document.querySelector('.main-content');
-console.log(width, height);
+// var width, height, mainContent, top, left;
+// //получаем данние о экране
+//
+// mainContent = document.querySelector('.main-content');
+//
+// function center(w,h){
+//     top = h/2*0.5;
+//     // left = w/2*0.5;
+//     // mainContent.style.left = left + 'px';
+//     mainContent.style.top = top + 'px'
+// }
+//
+// // width = screen.width;
+// height = screen.height;
+// center(width,height);
