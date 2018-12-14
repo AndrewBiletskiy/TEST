f7d568e8b3e6ec45313192b4284884f2a25c8087
BiletskiyAndery <pro100bejika@gmail.com>
2018-12-14 20:57:00 +0200

    add first sass

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -1,2 +1,9 @@
 import '../../assets/css/style.sass'
 import 'bootstrap'
+
+var width, height, mainContent;
+//получаем данние о экране
+width = screen.width;
+height = screen.height;
+mainContent = document.querySelector('.main-content');
+console.log(width, height);
