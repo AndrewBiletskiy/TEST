c9781ddbe397f16d0f1e6e10547d0f4d56a60500
BiletskiyAndery <pro100bejika@gmail.com>
2018-12-16 20:51:56 +0200

    ready index page

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -1,3 +1,31 @@
 import '../../assets/css/style.sass'
 import 'bootstrap'
+//change class onclick
+document.addEventListener('DOMContentLoaded', startF);
+
+function startF(){
+    vElements();
+    changeBtnHeader();
+
+}
+var spanHeader, btnHeader;
+
+function vElements() {
+    btnHeader = document.querySelector('.btn-navbar-toggler');
+    spanHeader = document.querySelector('.btn-navbar');
+
+}
+function changeClass(elFirst,elSecond,className) {
+//Change header class
+    function changeBtnClass() {
+        elSecond.classList.toggle(className)
+    }
+//events
+    elFirst.addEventListener('click',changeBtnClass);
+
+}
+function changeBtnHeader(){
+    changeClass(btnHeader, spanHeader, 'btn-close')
+
+}
 
