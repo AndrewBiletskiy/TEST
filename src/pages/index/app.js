01677e591cb5213b962c15d6808bfa4c326bbbd8
BiletskiyAndery <pro100bejika@gmail.com>
2018-12-16 02:15:59 +0200

    desktop version

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -1,18 +1,3 @@
 import '../../assets/css/style.sass'
 import 'bootstrap'
 
-// var width, height, mainContent, top, left;
-// //получаем данние о экране
-//
-// mainContent = document.querySelector('.main-content');
-//
-// function center(w,h){
-//     top = h/2*0.5;
-//     // left = w/2*0.5;
-//     // mainContent.style.left = left + 'px';
-//     mainContent.style.top = top + 'px'
-// }
-//
-// // width = screen.width;
-// height = screen.height;
-// center(width,height);
